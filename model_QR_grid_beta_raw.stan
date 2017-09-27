3e3143d4bf1c2036872a0f5dbad834560fe89b86
Philipp-Neubauer <neubauer.phil@gmail.com>
2017-09-28 11:03:49 +1300

    commit stuff to run on gorby

---
+++ model_QR_grid_beta_raw.stan
@@ -0,0 +1,86 @@
+data {
+  int<lower = 0> N_notzero;
+  int<lower = 0> N_groups;
+  int<lower = 0> N_islands;
+  int<lower = 0> N_grid;
+  int<lower = 0> N;
+  int<lower = 0> K;
+  vector<lower = 0, upper = 1>[N_notzero] IMEAN;
+  int<lower = 0, upper = 1> ZEROONE[N];
+  int<lower = 0> ii_notzero[N_notzero];
+  matrix[N, K] COVS;
+  int<lower = 1, upper = N_islands> ISLAND[N];
+  int<lower = 1, upper = N_groups> GROUP[N];
+  int<lower = 1, upper = N> GRID[N];
+}
+transformed data {
+  matrix[N, K] Q_ast;
+  matrix[K, K] R_ast;
+  matrix[K, K] R_ast_inverse;
+  
+  // thin and scale the QR decomposition
+  Q_ast = qr_Q(COVS)[, 1:K] * sqrt(N - 1);
+  R_ast = qr_R(COVS)[1:K, ] / sqrt(N - 1);
+  R_ast_inverse = inverse(R_ast);
+  
+}
+parameters {
+  vector[N_islands] island_fx;
+  vector<lower=0>[N_islands] iscale;
+  real<lower=0> gscale;
+  real zmean;
+  real scale;
+  vector[N_groups] group_fx;
+  real<lower=0> island_sig;
+  real<lower=0> group_sig;
+  vector[N_grid] grid_fx;
+  real<lower=0> grid_sig;
+  real omean;
+  vector[K] theta;
+}
+transformed parameters{
+ vector[N] imean;
+ vector<lower=0>[N_notzero] mu;
+ vector<lower=0>[N_notzero] a;
+ vector<lower=0>[N_notzero] b;
+  
+ imean = Q_ast*theta+island_sig*island_fx[ISLAND]+group_sig*group_fx[GROUP]+grid_sig*grid_fx[GRID];
+
+ mu = inv_logit(omean+imean[ii_notzero]);
+ a  =    mu  .* iscale[ISLAND[ii_notzero]];
+ b  = (1-mu) .* iscale[ISLAND[ii_notzero]];
+}
+model {
+  
+    // model for within grid sd, hierarchical across islands
+  iscale ~ cauchy(0,gscale);
+  gscale ~ cauchy(0,10);
+  
+  IMEAN ~ beta(a,b);
+  
+  //hurdle model for single measurement outcomes
+  ZEROONE ~ bernoulli_logit(zmean+scale*imean);
+  
+  //regression coeffs
+  omean ~ cauchy(0,10);
+  zmean ~ cauchy(0,10);
+  theta ~ cauchy(0,5);
+  scale ~ cauchy(0,5);
+  
+  island_fx ~ normal(0,1);
+  island_sig ~ cauchy(0,5);
+  group_fx ~ normal(0,1);
+  group_sig ~ cauchy(0,5);
+  grid_fx ~ normal(0,1);
+  grid_sig ~ cauchy(0,5);
+  
+}
+generated quantities{
+  vector[N] log_lik;
+  vector[K] beta;
+  beta = R_ast_inverse * theta; // coefficients
+  // beta_pdf for means and draws
+  for (i in 1:N) log_lik[i] =+ bernoulli_logit_lpmf(ZEROONE[i] | zmean+scale*imean[i]);
+  for (i in 1:N_notzero) log_lik[ii_notzero[i]] = beta_lpdf(IMEAN[i] | a[i], b[i]);
+  // Hurdle part of the likelihood for draws
+}
---
+++ reef-forcing_QR_grid_beta_raw_AR.r
@@ -0,0 +1,221 @@
+
+  require(readr)
+require(dplyr)
+require(rlang)
+require(cowplot)
+require(rstan)
+rstan_options(auto_write = TRUE)
+options(mc.cores = 6)
+
+
+get_vars <- function(data,var){
+  require(INLA)
+  adata <- data %>% filter(!is.na(TED_SUM),
+                           !is.na(SURF_SUM)) %>% 
+    mutate_(mm = var) %>%
+    mutate(mm=ifelse(N_OBS_CELL>0,mm/100,NA))
+  
+  pmax <- 0
+  island <- as.numeric(as.factor(adata$ISLAND))
+  islands <- unique(island)
+  nbbs <- list()
+  idds <- list()
+  for (i in 1:length(islands)) {
+    cat(i,'\n')
+    ids  <- adata$GRID_ID[island==islands[i]]
+    nbbs[[i]] <- lapply(1:length(unique(ids)), function(i) {
+      id <- unique(ids)[i]
+      if(id<max(ids)) {
+        if(!any(ids==(id+1))) NULL
+        else c(i,i+1)+pmax
+      }
+      else {c(1,i)+pmax}
+        
+      })
+    
+    idds[[i]] <- match(ids,unique(ids))+pmax
+    pmax <- pmax + length(unique(ids))
+    
+  }
+    
+  GRID <- do.call(c,idds)
+  N_grid <- length(unique(GRID))
+  nbs <- do.call(rbind,do.call(c,nbbs))
+  N_edges <- nrow(nbs)
+  #browser()
+
+  
+  COVS <- with(adata, as.matrix(
+    data.frame(DPOP      = scale(distant_hum_pop),
+               TED_SUM   = scale(TED_SUM)[,1],
+               SURF_SUM  = scale(SURF_SUM)[,1],
+               TED_INT   = scale(TED_SUM*distant_hum_pop)[,1],
+               SURF_INT  = scale(SURF_SUM*distant_hum_pop)[,1])))
+  
+    #browser()
+  
+  with(adata,list(
+    scaling_factor = 0.5,
+    node1     = nbs[,1],
+    node2     = nbs[,2],
+    N_edges   = N_edges,
+    ZEROONE   = ifelse(mm[!is.na(mm)]>0,1,0),
+    N_notzero = sum(mm>0,na.rm=T),
+    N_zeroone = sum(!is.na(mm)),
+    N_groups  = length(unique(group)),
+    N_islands = length(unique(ISLAND)),
+    N         = nrow(COVS),
+    K         = ncol(COVS),
+    ii_notzero= which(mm>0),
+    ii_zeroone= which(!is.na(mm)),
+    ii_map    = which(mm[which(!is.na(mm))]>0),
+    IMEAN     = mm[which(mm>0)],
+    COVS      = COVS,
+    ISLAND    = as.numeric(as.factor(ISLAND)),
+    GROUP     = as.numeric(as.factor(group)),
+    GRID      = GRID,
+    N_grid    = length(unique(GRID))
+    ))
+  
+}
+
+
+
+cdata <- read_csv('./model/data/ENTIRE_CLIMATOLOGY_ALL_ISLANDS.csv')
+rdata <- read_csv('./model/data/RAW_BENTHIC_ALL_ISLANDS.csv')
+
+all_data <- inner_join(rdata,cdata)
+
+## MACROALGAE
+
+VAR <- 'MACRO'
+model_data <- get_vars(all_data,VAR)
+
+fit_MAR <- stan('model_QR_grid_beta_raw_AR.stan', 
+              data = model_data, 
+              iter = 1000,
+              chains = 3,
+              thin = 1,
+              warmup = 500,
+              #init = inits,
+              pars = c('island_sig',
+                       'group_sig',
+                       'sigma',
+                       'grid_sig',
+                       'beta',
+                       'theta',
+                       'gscale',
+                       'iscale',
+                       'scale',
+                       'zmean',
+                       #'log_lik',
+                       'omean'), 
+              control = list(max_treedepth = 13, adapt_delta=0.8),
+              verbose = T)
+
+save(fit_MAR,file='ARfit_raw_macroalgae.Rdata')
+
+stan_trace(fit_MAR, 
+          pars=c('omean','sigma','grid_sig','beta','lp__','scale','zmean','island_sig','group_sig'), 
+          include=T)
+
+stan_plot(fit_MAR, 
+          pars=c('beta'), 
+          include=T, 
+          show_density = TRUE, 
+          fill_color = "lightblue") + 
+  theme_cowplot() + 
+  geom_vline(aes(xintercept=0), linetype=2, alpha=0.4)+
+  xlab('Effect size') + 
+  ylab('')
+
+## CCA
+
+VAR <- 'CCA'
+model_data <- get_vars(all_data,VAR)
+
+fit_CCAR <- stan('model_QR_grid_beta_raw_AR.stan', 
+              data = model_data, 
+              iter = 1000,
+              chains = 3,
+              thin = 1,
+              warmup = 500,
+              #init = inits,
+              pars = c('island_sig',
+                       'group_sig',
+                       'sigma',
+                       'grid_sig',
+                       'beta',
+                       'gscale',
+                       'iscale',
+                       'scale',
+                       #'log_lik',
+                       'omean',
+                       'zmean'), 
+              control = list(max_treedepth = 13, adapt_delta=0.8),
+              verbose = T)
+
+save(fit_CCAR,file='ARfit_raw_cca.Rdata')
+
+
+
+stan_trace(fit_CCAR, 
+          pars=c('omean','sigma','grid_sig','beta','lp__','scale','zmean','island_sig','group_sig'), 
+          include=T)
+
+stan_plot(fit_CCAR, 
+          pars=c('beta'), 
+          include=T, 
+          show_density = TRUE, 
+          fill_color = "lightblue") + 
+  theme_cowplot() + 
+  geom_vline(aes(xintercept=0), linetype=2, alpha=0.4)+
+  xlab('Effect size') + 
+  ylab('')
+
+
+## CORAL
+
+VAR <- 'CORAL'
+model_data <- get_vars(all_data,VAR)
+
+fit_CAR <- stan('model_QR_grid_beta_raw_AR.stan', 
+              data = model_data, 
+              iter = 1000,
+              chains = 3,
+              thin = 1,
+              warmup = 500,
+              #init = inits,
+              pars = c('island_sig',
+                       'group_sig',
+                       'sigma',
+                       'grid_sig',
+                       'beta',
+                       'gscale',
+                       'iscale',
+                       'scale',
+                       #'log_lik',
+                       'omean',
+                       'zmean'), 
+              control = list(max_treedepth = 13, adapt_delta=0.8),
+              verbose = T)
+
+save(fit_CAR,file='ARfit_raw_coral.Rdata')
+
+
+stan_trace(fit_CAR, 
+          pars=c('omean','sigma','grid_sig','beta','lp__','scale','zmean','island_sig','group_sig'), 
+          include=T)
+
+stan_plot(fit_CAR, 
+          pars=c('beta'), 
+          include=T, 
+          show_density = TRUE, 
+          fill_color = "lightblue") + 
+  theme_cowplot() + 
+  geom_vline(aes(xintercept=0), linetype=2, alpha=0.4)+
+  xlab('Effect size') + 
+  ylab('')
+
+
+
