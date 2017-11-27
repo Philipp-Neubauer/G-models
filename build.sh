b8d748e078df22ea473a11107479977e0cb73d85
Philipp-Neubauer <neubauer.phil@gmail.com>
2017-11-27 22:41:19 +1300

    try force install

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 set -ex
+Rscript -e 'options(repos = c(CRAN = "http://cran.rstudio.com"));install.packages("rmarkdown")'
 Rscript -e "rmarkdown::render('MS_results_INLA_wint.Rmd')"
 Rscript -e "rmarkdown::render('MS_results_INLA.Rmd')"
 Rscript INLA_version.R
