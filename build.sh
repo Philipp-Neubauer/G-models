8959d106a9106a48393e6e85658ff24eee29e3a0
Philipp-Neubauer <neubauer.phil@gmail.com>
2017-11-27 22:45:42 +1300

    install pandoc

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 set -ex
 apt-get update
-apt-get install pandoc
+apt-get install -y pandoc
 Rscript -e 'options(repos = c(CRAN = "http://cran.rstudio.com"));install.packages("rmarkdown")'
 Rscript -e "rmarkdown::render('MS_results_INLA_wint.Rmd')"
 Rscript -e "rmarkdown::render('MS_results_INLA.Rmd')"
