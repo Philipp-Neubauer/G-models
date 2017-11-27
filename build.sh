5ffed1322714b9c61464f7df0c8e37dbada5747c
Philipp-Neubauer <neubauer.phil@gmail.com>
2017-11-28 07:23:18 +1300

    filename!

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ apt-get install -y pandoc
 Rscript -e 'options(repos = c(CRAN = "http://cran.rstudio.com"));install.packages("rmarkdown");install.packages("INLA", repos="https://inla.r-inla-download.org/R/testing", dep=TRUE)'
 Rscript -e "rmarkdown::render('MS_results_INLA_wint.Rmd')"
 Rscript -e "rmarkdown::render('MS_results_INLA.Rmd')"
-Rscript INLA_version.R
+Rscript ILNA_version.R
 
 cp *.Rdata /output/
 cp *.pdf /output/
